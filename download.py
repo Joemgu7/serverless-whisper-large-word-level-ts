54ca1d0d76bb4caefe7837589da7865a7a574a59
briguetjo <briguetjo@gmail.com>
2023-02-14 16:54:31 +0100

    remove arg from docker file

--- download.py
+++ download.py
@@ -1,13 +1,10 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface BERT model
-
 import whisper
-import os
 
 def download_model():
-    model_name = os.getenv("MODEL_NAME")
+    model_name = "large"
     model = whisper.load_model(model_name)
 
 if __name__ == "__main__":
